516f03992777ef5404e219cb234f7f11ef9e18f9
@Mikist <mikeochango3@gmail.com>
2024-11-10 15:00:30 +0300

    implementing Advanced Model Relationships in Django

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,14 +1,35 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
 # Query all books by a specific author.
-author = Author.objects.get(name='Mike')
-books = Book.objects.filter(author=author)
+
+# author = Author.objects.get(name='Mike')
+# books = Book.objects.filter(author=author)
+# def  get_author(author_name):
+#     author = Author.objects.get(name = author_name)
+#     print(f"Book by {author}")
+
+author_name = Author.objects.get(name='Mike')
+Author.objects.get(name=author_name)
+
+
+
+
+
 
 # List all books in a library.
+
 # books = Library.objects.all()
 # Library.objects.get(name='Mike  Absai Library')
 library_name = 'Mike  Absai Library'
 Library.objects.get(name=library_name)
+
+books = Book.objects.all()
 books.all()
+
+
+
+
+
+
 
 # Retrieve the librarian for a library.
