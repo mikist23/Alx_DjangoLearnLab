bab3c36e16c386e8817a5d11c08fa236ae47f463
@Mikist <mikeochango3@gmail.com>
2024-11-10 16:34:42 +0300

    Implementing User Authentication in Django

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView
 
+from django.views.generic.base import TemplateView
+from relationship_app.views import SignUpView
+
 urlpatterns = [
     path('books/', list_books, name = 'list_books'),
      path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+
+
+
+    path('register/',
+             TemplateView.as_view(template_name='relationship_app/register.html'),
+             name='register'),
+    path("signup/", SignUpView.as_view(), name="templates/relationship_app/signup"),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 
 
@@ -26,3 +29,10 @@ class LibraryDetailView(DetailView):
         library = self.get_object()
         context['book_list'] = library.books.all() 
         return context
+
+
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "relationship_app/signup.html"
