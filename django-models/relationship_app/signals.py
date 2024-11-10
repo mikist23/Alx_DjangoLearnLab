5d8d96f54ed95cc2cdbcdaf618750ff517231c34
@Mikist <mikeochango3@gmail.com>
2024-11-10 18:23:09 +0300

    Implement Role-Based Access Control in Django

--- django-models/relationship_app/signals.py
+++ django-models/relationship_app/signals.py
@@ -11,3 +11,11 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.userprofile.save()
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance, role='Librarians')@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance, role='Librarians')
