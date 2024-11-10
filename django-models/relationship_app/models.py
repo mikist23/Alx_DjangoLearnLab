08f1e6675b22889dcae01a30d1982c4af82caefe
@Mikist <mikeochango3@gmail.com>
2024-11-10 18:44:25 +0300

    Implementing Custom Permissions in Django

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,3 +1,75 @@
+# from django.db import models
+# from django.contrib.auth.models import User
+
+# # Create your models here.
+# class Author(models.Model):
+#     name = models.CharField(max_length=200)
+    
+#     def __str__(self):
+#         return self.name
+
+# class Book(models.Model):
+#     title = models.CharField(max_length=200)
+#     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+#     def __str__(self):
+#         return self.title
+
+# class Library(models.Model):
+#     name = models.CharField(max_length=200)
+#     books = models.ManyToManyField(Book)
+
+#     def __str__(self):
+#         return self.name
+
+# class Librarian(models.Model):
+#     name = models.CharField(max_length=200)
+#     library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+#     def __str__(self):
+#         return self.name
+    
+
+
+
+
+# class UserProfile(models.Model):
+#     ROLE_CHOICES = [
+#         ('Admins', "Admins"),
+#         ('Librarians', 'Librarians'),
+#         ('Member', 'Member'),
+#     ]
+    
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+
+#     def __str__(self):
+#         return f"{self.user.username} - {self.role}"
+
+
+
+
+
+
+
+# class Book(models.Model):
+#     title = models.CharField(max_length=200)
+#     author = models.CharField(max_length=200)
+#     published_date = models.DateField()
+
+#     class Meta:
+#         permissions = [
+#             ("can_add_book", "Can add book"),
+#             ("can_change_book", "Can change book"),
+#             ("can_delete_book", "Can delete book"),
+#         ]
+    
+#     def __str__(self):
+#         return self.title
+
+
+
+
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -10,8 +82,16 @@ class Author(models.Model):
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    author = models.CharField(max_length=200)  # Use CharField if not referencing the Author model directly
+    published_date = models.DateField()
 
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
+    
     def __str__(self):
         return self.title
 
@@ -28,10 +108,7 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
-    
-
-
-
+  
 
 class UserProfile(models.Model):
     ROLE_CHOICES = [
@@ -45,24 +122,3 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
-
-
-
-
-
-
-
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
-    published_date = models.DateField()
-
-    class Meta:
-        permissions = [
-            ("can_add_book", "Can add book"),
-            ("can_change_book", "Can change book"),
-            ("can_delete_book", "Can delete book"),
-        ]
-    
-    def __str__(self):
-        return self.title
