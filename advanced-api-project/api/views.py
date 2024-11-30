ac791a677a885509c0f26d4b1be6129ef11310b3
@Mikist <mikeochango3@gmail.com>
2024-11-30 14:08:36 +0300

    created the generics views components

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from .models import Book, Author
+from rest_framework import generics
+from .serializers import BookSerializer, AuthorSerializer
 
 # Create your views here.
+
+class CustomBookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class CustomBookDetalView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = 'id'
+
+class CustomBookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class CustomBookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = 'id'
+
+class CustomBookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = 'id'
