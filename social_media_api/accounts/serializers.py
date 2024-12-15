884bcc7968e1c9e7f0d356f099d7cf0722976643
@Mikist <mikeochango3@gmail.com>
2024-12-15 18:38:59 +0300

    user profile serializer

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,23 +1,25 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
 # CLASS FOR REGISTERING NEW USERS
 class RegistrationSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField()
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password', 'bio', 'profile_picture']
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
              username = validated_data['username'],
              email = validated_data.get('email', ''),
              password= validated_data['password'],
              bio = validated_data.get('bio',''),
              profile_picture = validated_data.get('profile_picture', None),
+             token = Token.objects.create()
         )
 
         return user
@@ -39,3 +41,10 @@ class DeleteUserSerializer(serializers.ModelSerializer):
 # CLASS FOR TOKEN GENERATION
 class TokenSerializer(serializers.ModelSerializer):
     token = serializers.CharField(read_only=True)
+
+# CLASS FOR USER PROFILE
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'bio', 'profile_picture']
+        read_only_fields = ['id', 'username']  # Prevent updates to certain fields
