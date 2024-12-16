3b9f8c32060b32880247d1dc09d683f6ec630d5f
@Mikist <mikeochango3@gmail.com>
2024-12-16 19:13:15 +0300

    Implementing Posts and Comments Functionality

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,7 +1,39 @@
-from django.urls import path
+from django.urls import path, include
+from .views import PostViewSet, CommentViewSet
+from rest_framework.routers import DefaultRouter
+
+ 
+
+# -----------_____________USING VIEWSETS ___________________---------------------
+
+router = DefaultRouter()
+
+router.register(r'posts', PostViewSet, basename='post')
+router.register(r'comments', CommentViewSet, basename='comment')
 
 
 urlpatterns = [
 
-    
+   
+    path('', include(router.urls)),
 ]
+
+
+
+
+
+# -----------_____________USING GENERICS ___________________---------------------
+# from django.urls import path
+# from .views import (
+#     PostListCreateView, 
+#     PostDetailView, 
+#     CommentListCreateView, 
+#     CommentDetailView
+# )
+
+# urlpatterns = [
+#     path('posts/', PostListCreateView.as_view(), name='post-list-create'),
+#     path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+#     path('comments/', CommentListCreateView.as_view(), name='comment-list-create'),
+#     path('comments/<int:pk>/', CommentDetailView.as_view(), name='comment-detail'),
+# ]
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from .serializers import RegistrationSerializer, PostSerializer, CommentSerializer, ListUsersSerializer, TokenSerializer, DeleteUserSerializer, UserProfileSerializer
-from .models import CustomUser, Post, Comment
+from .serializers import  PostSerializer, CommentSerializer
+from .models import   Post, Comment
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
@@ -19,6 +19,7 @@ from rest_framework.exceptions import PermissionDenied
 
 # __________---------------_______  POST COMMENT VIEWS  ____________ ----------
 
+# -----------_____________USING VIEWSETS___________________---------------------
 class PostViewSet(viewsets.ModelViewSet):
     """
         Viewset for CRUD operations on POst model.
@@ -82,3 +83,109 @@ class CommentViewSet(viewsets.ModelViewSet):
         if instance.author != self.request.user:
             raise PermissionDenied("You do not have permission to delete this comment.")
         instance.delete()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# -----------_____________USING GENERICS ___________________---------------------
+# from rest_framework import generics, permissions
+# from rest_framework.exceptions import PermissionDenied
+# from .models import Post, Comment
+# from .serializers import PostSerializer, CommentSerializer
+
+
+# # POST Views
+# class PostListCreateView(generics.ListCreateAPIView):
+#     """
+#     List all posts or create a new post.
+#     """
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+#     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+#     def perform_create(self, serializer):
+#         """
+#         Automatically set the author to the logged-in user when creating a post.
+#         """
+#         serializer.save(author=self.request.user)
+
+
+# class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     """
+#     Retrieve, update, or delete a post.
+#     """
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+#     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+#     def perform_update(self, serializer):
+#         """
+#         Ensure only the author can update the post.
+#         """
+#         post = self.get_object()
+#         if post.author != self.request.user:
+#             raise PermissionDenied("You do not have permission to edit this post.")
+#         serializer.save()
+
+#     def perform_destroy(self, instance):
+#         """
+#         Ensure only the author can delete the post.
+#         """
+#         if instance.author != self.request.user:
+#             raise PermissionDenied("You do not have permission to delete this post.")
+#         instance.delete()
+
+
+# # COMMENT Views
+# class CommentListCreateView(generics.ListCreateAPIView):
+#     """
+#     List all comments or create a new comment.
+#     """
+#     queryset = Comment.objects.all()
+#     serializer_class = CommentSerializer
+#     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+#     def perform_create(self, serializer):
+#         """
+#         Automatically set the author to the logged-in user when creating a comment.
+#         """
+#         serializer.save(author=self.request.user)
+
+
+# class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     """
+#     Retrieve, update, or delete a comment.
+#     """
+#     queryset = Comment.objects.all()
+#     serializer_class = CommentSerializer
+#     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+#     def perform_update(self, serializer):
+#         """
+#         Ensure only the author can update the comment.
+#         """
+#         comment = self.get_object()
+#         if comment.author != self.request.user:
+#             raise PermissionDenied("You do not have permission to edit this comment.")
+#         serializer.save()
+
+#     def perform_destroy(self, instance):
+#         """
+#         Ensure only the author can delete the comment.
+#         """
+#         if instance.author != self.request.user:
+#             raise PermissionDenied("You do not have permission to delete this comment.")
+#         instance.delete()
