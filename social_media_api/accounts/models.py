958c664c5b5d1922b32c46a0f370442fa586e707
@Mikist <mikeochango3@gmail.com>
2024-12-16 13:00:42 +0300

    Ad post and comment models

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+User = get_user_model()
 
+# MODEL CLASS FOR CREATING CUSTOM USER MODEL
 class CustomUser(AbstractUser):
     bio = models.TextField(blank=True, null=True)
     profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
@@ -10,3 +13,29 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+    
+
+# MODEL CLASS FOR POSTS
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+
+# MODEL CLASS FOR COMMENT
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.post}"
