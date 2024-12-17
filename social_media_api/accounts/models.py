2d260387c1cc368d919504ead6d8ff3647449bbc
@Mikist <mikeochango3@gmail.com>
2024-12-17 16:06:02 +0300

    update custome user model

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -11,7 +11,8 @@ from django.utils import timezone
 class CustomUser(AbstractUser):
     bio = models.TextField(blank=True, null=True)
     profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
-    followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
+    followers = models.ManyToManyField('self', symmetrical=False, related_name='user_followers', blank=True)
+    following = models.ManyToManyField('self', symmetrical=False, related_name='user_following', blank=True)
 
     def __str__(self):
         return self.username
