99730ac976c2a074f8f6b08df1895648b89ab3de
@Mikist <mikeochango3@gmail.com>
2024-12-18 17:23:16 +0300

    Add the views for motifications

---
+++ social_media_api/notifications/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from .models import Notification
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from .serializers import NotificationSerializer
+
+# Create your views here.
+
+class ListNotificationView(ListAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = NotificationSerializer
+
+    def get_queryset(self):
+        # Fetch notifications for the logged in user
+        return Notification.objects.filter(recipient=self.request.user).order_by('-timestap')
+    
