ee8dc5bfc9963ee0e1e08332ed4fd861b73162a4
@Mikist <mikeochango3@gmail.com>
2024-12-18 17:40:15 +0300

    Add urls implementation for like and notifications

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import PostViewSet, CommentViewSet, UserFeedView
+from .views import PostViewSet, CommentViewSet, UserFeedView, LikePostView, UnlikingPostView
 from rest_framework.routers import DefaultRouter
 
  
@@ -14,9 +14,15 @@ router.register(r'comments', CommentViewSet, basename='comment')
 
 urlpatterns = [
 
-   
+#    ++++++++++--URLS FOR POST +++++----
     path('', include(router.urls)),
     path('feed/', UserFeedView.as_view(), name='user_feed'),
+
+    #    ++++++++++--URLS FOR LIKES +++++----
+    path('posts/<int:pk>/like/', LikePostView.as_view(), name='like-post'),
+    path('posts/<int:pk>/unlike/', UnlikingPostView.as_view(), name='unlike-post'),
+
+
 ]
 
 
